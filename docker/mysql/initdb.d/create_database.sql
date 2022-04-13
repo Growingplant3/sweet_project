0f5d563881de13c2718dbfc2558413bf1d605d36
Yoshinori Shimoda <ys@platinum.local>
2022-04-14 03:43:43 +0900

    first commit

---
+++ docker/mysql/initdb.d/create_database.sql
@@ -0,0 +1,1 @@
+CREATE DATABASE IF NOT EXISTS sweet_test_dev;
---
+++ project/resources/views/web/common/top.blade.php
@@ -0,0 +1,9 @@
+@include("web.common.head")
+@include("web.common.header")
+
+@include("web.common.top.section1")
+@include("web.common.top.section2")
+@include("web.common.top.section3")
+@include("web.common.top.section4")
+
+@include("web.common.footer")
