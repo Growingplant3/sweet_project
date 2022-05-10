d7552d5bb65c266e35a657a931c70723088b8358
Yoshinori Shimoda <ys@platinum.local>
2022-05-11 03:25:39 +0900

    viewファイルの階層を変更

--- project/resources/views/web/common/top.blade.php
+++
@@ -1,9 +0,0 @@
-@include("web.common.head")
-@include("web.common.header")
-
-@include("web.common.top.section1")
-@include("web.common.top.section2")
-@include("web.common.top.section3")
-@include("web.common.top.section4")
-
-@include("web.common.footer")
