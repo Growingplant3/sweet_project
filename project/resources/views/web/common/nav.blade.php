c4767bb6a9f70c95ee8d84c46145818f1ec60e9c
Yoshinori Shimoda <ys@platinum.local>
2022-05-11 03:27:19 +0900

    トップページと制作物へのリンクを整備

--- project/resources/views/web/common/nav.blade.php
+++ project/resources/views/web/common/nav.blade.php
@@ -1,7 +1,8 @@
 <!-- Navigation-->
-<nav class="navbar navbar-light bg-light static-top">
+<nav class="navbar navbar-light bg-primary static-top">
 	<div class="container">
-		<a class="navbar-brand" href="{{ url('/') }}">トップ</a>
+		<a class="navbar-brand" href={{ route('default') }}>トップページ</a>
+        <a class="navbar-brand" href={{ route('work') }}>制作物一覧</a>
 		<a class="btn btn-primary" href="{{ url('/about') }}">会社概要</a>
 	</div>
 </nav>
